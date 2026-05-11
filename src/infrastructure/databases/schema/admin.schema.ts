e8246fb1c56d9edb7b5859dac97c5699c8376ed9
risansadik <risansadik@gmail.com>
2026-05-11 11:02:59 +0530

    implemented schemas

---
+++ src/infrastructure/databases/schema/admin.schema.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema, type Document } from "mongoose";
+
+export interface IAdminDocument extends Document {
+  email: string;
+  password: string;
+}
+
+const AdminSchema = new Schema<IAdminDocument>(
+  {
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const AdminModel = mongoose.model<IAdminDocument>("Admin", AdminSchema);
