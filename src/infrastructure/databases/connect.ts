76045b106834206afbe06657727ea3ce36912700
risansadik <risansadik@gmail.com>
2026-05-11 13:03:05 +0530

    connected mongoDB and modified entry points

---
+++ src/infrastructure/databases/connect.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { logger } from "../../shared/utils/logger.js";
+
+export const connectDB = async (): Promise<void> => {
+  const mongoUri = process.env.MONGO_URI;
+  if (!mongoUri) throw new Error("MONGO_URI is not defined");
+
+  try {
+    await mongoose.connect(mongoUri);
+    logger.info("MongoDB connected successfully");
+  } catch (error) {
+    logger.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
+};
