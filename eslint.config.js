56a9db36063cdb1d67648d2188391b508a5cfa24
risansadik <risansadik@gmail.com>
2026-07-13 10:13:22 +0530

    fix: exclude commitlint config from eslint checks

--- eslint.config.js
+++ eslint.config.js
@@ -12,6 +12,6 @@ export default tseslint.config(
     },
   },
   {
-    ignores: ["dist/", "node_modules/", "uploads/"],
+    ignores: ["dist/", "node_modules/", "uploads/", "commitlint.config.cjs"],
   }
 );
